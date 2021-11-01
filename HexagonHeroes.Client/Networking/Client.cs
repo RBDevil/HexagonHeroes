f6eda2620620e1e42297ef827bff4bc2587d6940
PRO54B <PRO54B@stud.uni-obuda.hu>
2021-11-01 16:49:23 +0100

    Implement server and client

---
+++ HexagonHeroes.Client/Networking/Client.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using Lidgren.Network;
+using Packets;
+
+namespace Networking
+{
+	public class PlayerPosition
+	{
+		public float X { get; set; }
+		public float Y { get; set; }
+	}
+
+	public class Client
+	{
+		public NetClient client { get; set; }
+
+		public Client(int port, string server, string serverName)
+		{
+			var config = new NetPeerConfiguration(serverName)
+			{
+				AutoFlushSendQueue = false
+			};
+
+			client = new NetClient(config);
+			client.RegisterReceivedCallback(new SendOrPostCallback(ReceiveMessage));
+			client.Start();
+
+			client.Connect(server, port);
+		}
+
+		public void ReceiveMessage(object peer)
+		{
+			NetIncomingMessage message;
+
+			while ((message = client.ReadMessage()) != null)
+			{
+				switch (message.MessageType)
+				{
+					case NetIncomingMessageType.Data:
+						var packetType = (int)message.ReadByte();
+
+						Packet packet;
+
+						switch (packetType)
+						{
+							case (int)PacketTypes.LocalPlayerPacket:
+
+								break;
+							case (int)PacketTypes.PlayerDisconnectsPacket:
+
+								break;
+							case (int)PacketTypes.PositionPacket:
+
+								break;
+							case (int)PacketTypes.SpawnPacket:
+
+								break;
+						}
+
+						break;
+					case NetIncomingMessageType.DebugMessage:
+					case NetIncomingMessageType.ErrorMessage:
+					case NetIncomingMessageType.WarningMessage:
+					case NetIncomingMessageType.VerboseDebugMessage:
+						string text = message.ReadString();
+						break;
+					case NetIncomingMessageType.StatusChanged:
+						NetConnectionStatus status = (NetConnectionStatus)message.ReadByte();
+						string reason = message.ReadString();
+						break;
+					default:
+						break;
+				}
+
+				client.Recycle(message);
+			}
+		}
+	}
+}
---
+++ HexagonHeroes.Server/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Networking;
+
+namespace HexagonHeroes.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Networking.Server server = new Networking.Server();
+        }
+    }
+}
